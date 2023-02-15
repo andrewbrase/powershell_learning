02d8fcf5a23c6fcba28a1bfda0ed5886f0e4c959
Brase <brasea@wwt.com>
2023-02-15 09:00:22 -0600

    learning powershell first projects

--- 01_thisYear.ps1
+++ 01_thisYear.ps1
@@ -1,3 +1,13 @@
 ﻿$dateTime = get-date
 $thisYear = $dateTime.Year
 write-host "The year is $thisYear!"
+
+<#
+PowerShell is a scripting language and shell built on top of the .NET framework.
+
+PowerShell provides a command-line interface (CLI) for interacting with the operating system, 
+as well as a scripting language for writing scripts to automate common tasks.
+
+One of the key features of PowerShell is its ability to work with .NET objects and APIs, 
+which makes it a powerful tool for managing Windows systems.
+#>
