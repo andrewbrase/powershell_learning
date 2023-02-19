85ea03b2a04b1374bfa14ef4bc3f6c03104b1295
Brase <brasea@wwt.com>
2023-02-18 21:00:46 -0600

    working with powershell variables

--- 05_variables.ps1
+++ 05_variables.ps1
@@ -1,1 +1,19 @@
-$thisprocess = Get-Process
+
+# Get-Process
+# $process = Get-Process
+
+# -----------------------------
+# not using variable
+
+# to get these two sets of info, you have to load get-process twice
+# instead of doing that, you can just save the object as a variable to use
+# later
+
+Get-Process | Where-Object {$_.CPU -gt 1000} #find processes keeping the CPU busy
+Get-Process | Sort-Object WorkingSet64 -Descending #sort processes by memory usage
+# -----------------------------
+# using variable
+$processes = Get-Process
+$processes | Where-Object {$_.CPU -gt 1000}
+$processes | Sort-Object WorkingSet64 -Descending
+# -----------------------------
